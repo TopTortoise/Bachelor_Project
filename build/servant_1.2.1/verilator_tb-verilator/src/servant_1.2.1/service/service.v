cfe6b898b784ac70a6b4e3f8477cbdda53493e58
paul <paul.b0o3721@gmail.com>
2023-10-03 14:18:07 +0200

    fixed conflicts

--- build/servant_1.2.1/verilator_tb-verilator/src/servant_1.2.1/service/service.v
+++ build/servant_1.2.1/verilator_tb-verilator/src/servant_1.2.1/service/service.v
@@ -117,6 +117,7 @@ module service
     .o_wb_rdt(to_pc)
   );
 
+<<<<<<< HEAD
   //queue queue (
   //   .i_wb_clk(i_clk),
   //   .i_wb_dat(from_ble),
@@ -129,6 +130,20 @@ module service
   //);
   //assign to_pc = i_data;
 
+=======
+  queue queue (
+     .i_wb_clk(i_clk),
+     .i_wb_dat(from_ble),
+     .i_wb_mem_cyc(wb_mem_cyc),
+     .i_wb_mem_we(wb_mem_we),
+     .i_rx_done(rx_done),
+     .i_wb_rst(wb_rst),
+     .o_wb_rdt(queue_dat)
+
+  );
+  //assign to_pc = i_data;
+
+>>>>>>> refs/remotes/origin/main
   reg [31:0] ble_data_adr = adr_LL;
 
   //united ram signals
@@ -139,8 +154,13 @@ module service
   wire [31:0] adr;
 
 
+<<<<<<< HEAD
   //wire [7:0] queue_dat;
   //wire write_queue;
+=======
+  wire [7:0] queue_dat;
+  wire write_queue;
+>>>>>>> refs/remotes/origin/main
 
   always @(posedge i_clk) begin
     if(wb_rst)begin
@@ -155,6 +175,7 @@ module service
     else
     begin
     //for testing sending data to pc
+<<<<<<< HEAD
     
     pc_active <= 0;
     
@@ -162,6 +183,14 @@ module service
     //  write_queue <= 1;
     //else if (!wb_mem_we&!wb_mem_cyc)
     //  write_queue <= 0;
+=======
+    pc_active <= 0;
+    
+    if(rx_done)
+      write_queue <= 1;
+    else if (!wb_mem_we&!wb_mem_cyc)
+      write_queue <= 0;
+>>>>>>> refs/remotes/origin/main
     //
     case (wb_mem_adr)
       'h00A00000:// tx -> pc
@@ -191,6 +220,7 @@ module service
     end
   end
 
+<<<<<<< HEAD
     assign  adr = rx_done? ble_data_adr           : wb_mem_adr;
     assign  cyc = rx_done? 1'b1                   : wb_mem_cyc;
     assign  we  = rx_done? 1'b1                   : wb_mem_we;
@@ -200,6 +230,17 @@ module service
    // assign  write_queue = rx_done|!wb_mem_we&!wb_mem_cyc?
 
     //assign q1 = 4'b0001;
+=======
+    assign  adr = !wb_mem_we&!wb_mem_cyc&write_queue? ble_data_adr   : wb_mem_adr;
+    assign  cyc = !wb_mem_we&!wb_mem_cyc&write_queue? 1'b1     : wb_mem_cyc;
+    assign  we  = !wb_mem_we&!wb_mem_cyc&write_queue? 1'b1     : wb_mem_we;
+    assign  sel = !wb_mem_we&!wb_mem_cyc&write_queue? 4'b1111  : wb_mem_sel;
+    assign  dat = !wb_mem_we&!wb_mem_cyc&write_queue? queue_dat : wb_mem_dat;
+
+   // assign  write_queue = rx_done|!wb_mem_we&!wb_mem_cyc?
+
+    assign q1 = 4'b0001;
+>>>>>>> refs/remotes/origin/main
   
 
 endmodule
