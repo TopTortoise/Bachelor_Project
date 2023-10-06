310c3bbe984612352b516c2997a8f90a08f75810
TopTortoise <paul.b0o3721@gmail.com>
2023-10-06 20:45:47 +0200

    update servant_tb

--- fusesoc_libraries/serv/bench/servant_tb.cpp
+++ fusesoc_libraries/serv/bench/servant_tb.cpp
@@ -8,11 +8,12 @@
 using namespace std;
 
 static bool done;
-                          //NAME,RSSI\n
+// NAME,RSSI\n
 
 vluint64_t main_time = 0; // Current simulation time
 // This is a 64-bit integer to reduce wrap over issues and
 // allow modulus.  You can also use a double, if you wish.
+static int baud_t = 208;
 
 double sc_time_stamp()
 {                   // Called by $time in Verilog
@@ -50,18 +51,18 @@ typedef struct
 
 void uart_init(uart_context_t *context, uint32_t baud_rate)
 {
-  context->baud_t =  208;//12000000/baud_rate;
+  context->baud_t = baud_t; // 12000000/baud_rate;
   context->state = 0;
 }
 
 bool do_uart(uart_context_t *context, bool rx)
 {
-  if (context->state == 0)//start
+  if (context->state == 0) // start
   {
     if (rx)
       context->state++;
   }
-  else if (context->state == 1)//wait for 0 from input IDLE
+  else if (context->state == 1) // wait for 0 from input IDLE
   {
     if (!rx)
     {
@@ -69,7 +70,7 @@ bool do_uart(uart_context_t *context, bool rx)
       context->state++;
     }
   }
-  else if (context->state == 2)//init last_update and ch START
+  else if (context->state == 2) // init last_update and ch START
   {
     if (main_time > context->last_update)
     {
@@ -78,7 +79,7 @@ bool do_uart(uart_context_t *context, bool rx)
       context->state++;
     }
   }
-  else if (context->state < 11)//store byte
+  else if (context->state < 11) // store byte
   {
     if (main_time > context->last_update)
     {
@@ -102,38 +103,50 @@ bool do_uart(uart_context_t *context, bool rx)
 int state = 0;
 int last_update = 0;
 
-static int baud_t =208;
-bool send_uart(Vservant_sim *top,char ch){
- if (state==0)//IDLE
- {
-  top->i_data = 1;                  
-  last_update = main_time + baud_t;
-  state++;
- }else if (state==1)
- {
-  top->i_data = 0;
-  if(main_time > last_update)
+bool send_uart(Vservant_sim *top, char ch)
+{
+  if (state == 0) // IDLE
   {
-    last_update += baud_t;
+    top->i_data = 1;
+    last_update = main_time + baud_t;
     state++;
   }
-  else if (state < 10)
-   if(main_time)
+  else if (state == 1)
   {
-    /* code */
+    top->i_data = 0;
+    if (main_time > last_update)
+    {
+      last_update += baud_t;
+      state++;
+    }
   }
-  
-  /* code */
- }
- 
+  else if (state < 10)
+  {
+    top->i_data = (ch >> state - 2) & 0x01;
+    if (main_time > last_update)
+    {
+      printf("\n%d", top->i_data);
+      last_update += baud_t;
+      state++;
+    }
+  }
+  else
+  {
+      top->i_data = 1;
+    if (main_time > last_update)
+    {
+      state = 0;
+      return true;
+    }
+  }
 
- 
+  return false;
 }
 
 int main(int argc, char **argv, char **env)
 {
   int baud_rate = 0;
-  char* BLE_data = "CMD>\nScanning BLE1,1\nBLE2,2\nBLE4,4\nBLE8,9\nBLE1,8\nBLE,0\0";
+  char *BLE_data = "CMD>\nScanning BLE1,1\nBLE2,2\nBLE4,4\nBLE8,9\nBLE1,8\nBLE,0\0";
 
   gpio_context_t gpio_context;
   uart_context_t uart_context;
@@ -180,6 +193,7 @@ int main(int argc, char **argv, char **env)
   bool q = top->q;
 
   int counter = 0;
+  bool rx_done;
   while (!(done || Verilated::gotFinish()))
   {
 
@@ -193,29 +207,32 @@ int main(int argc, char **argv, char **env)
     if (dump)
       tfp->dump(main_time);
 
-  
     if (do_uart(&uart_context, top->o_data))
     {
+      rx_done = true;
       putchar(uart_context.ch);
       fflush(stdout);
-    //  if (uart_context.ch != 0) printf("%c \n", uart_context.ch);
+      //  if (uart_context.ch != 0) printf("%c \n", uart_context.ch);
     }
-    if(uart_context.ch==0x0d && *BLE_data != '\0') {
-     // if(send_uart(top, *BLE_data)) BLE_data++;
-      //printf("%c",*BLE_data);
-    }// printf("hello\n");
+    if (uart_context.ch == 0x0d && *BLE_data != '\0'  && rx_done)
+    {
 
-      //counter++;
+      if (send_uart(top, *BLE_data))
+        BLE_data++;
+      // printf("%c",*BLE_data);
+    } // printf("hello\n");
+
+    // counter++;
     if (timeout && (main_time >= timeout))
     {
       printf("Timeout: Exiting at time %lu\n", main_time);
       done = true;
     }
-    //if (counter == 40000)
-    //  break;
+    // if (counter == 40000)
+    //   break;
     top->wb_clk = !top->wb_clk;
 
-    main_time += 1;//83333/2;
+    main_time += 1; // 83333/2;
   }
   if (tfp)
     tfp->close();
