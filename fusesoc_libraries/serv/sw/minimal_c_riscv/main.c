f4dffaef22b1e2c39f8af1b093760a96c27bc317
TopTortoise <paul.b00o3721@gmail.com>
2023-11-01 11:59:08 +0100

    refined analyze function

--- fusesoc_libraries/serv/sw/minimal_c_riscv/main.c
+++ fusesoc_libraries/serv/sw/minimal_c_riscv/main.c
@@ -18,8 +18,8 @@ const char END = '-';
 const char SEND = 0x0d;
 
 //DELAYS
-const int DELAY_T = 0x08A8;//0x01A8
-const unsigned int SCAN_DELAY_T = 0xB71B00;
+const int DELAY_T = 0x03A8;//0x01A8
+const unsigned int SCAN_DELAY_T = 0xB71B0;
 
 
 void delay(int delay_t);
@@ -40,19 +40,23 @@ int main() {
     delay(DELAY_T);
     *UART_BLE = SEND;
     delay(DELAY_T);
+     *UART_PC = 'R';
+    delay(DELAY_T); 
     //F
     *UART_BLE = SCAN; 
     delay(DELAY_T);
     *UART_BLE = SEND;
     delay(SCAN_DELAY_T);
-    
+     *UART_PC = 'R';
+    delay(DELAY_T); 
     //X
     *UART_BLE = STOP; 
     delay(DELAY_T);
     *UART_BLE = SEND;
     delay(DELAY_T);
 
-
+    *UART_PC = 'R';
+    delay(DELAY_T); 
     //---
     *UART_BLE = END; 
     delay(DELAY_T);
@@ -74,17 +78,26 @@ int main() {
     delay(DELAY_T); 
     *UART_PC = 'A';
     delay(DELAY_T); 
-    analyze();
-    *UART_PC = '\n';
+    *UART_PC = 'N';
     delay(DELAY_T); 
-    *UART_PC = 'R';
+    *UART_PC = 'A';
     delay(DELAY_T); 
-    read();
-
-    *UART_PC = '\n';
+    *UART_PC = 'L';
+    delay(DELAY_T); 
+    *UART_PC = 'Y';
+    delay(DELAY_T); 
+    *UART_PC = 'Z';
+    delay(DELAY_T);
+    *UART_PC = 'E';
     delay(DELAY_T);
     *UART_PC = 'D';
     delay(DELAY_T);
+    *UART_PC = '\n';
+    delay(DELAY_T); 
+    *UART_PC = 0x0d;
+    delay(DELAY_T); 
+    analyze();
+    read();
 
     while(1);
   
@@ -98,7 +111,7 @@ int main() {
 */
 void /* __attribute__((optimize("Os"))) */ read(){
   char c;
-  while ((c = *DATA), c!='\0')
+  while ((c = *DATA), c!=0 )
   {
     *UART_PC = c;
     delay(DELAY_T);
@@ -125,15 +138,16 @@ void analyze(){
   {
     DATA++;
   }
-    *UART_PC = 'S';
-    delay(DELAY_T); 
-  while (*DATA != 0)
+
+
+  while (*DATA != 0)//analyze until end of data
   {
     
-    while(*DATA != '%'){
+    while(*DATA != '%')//skip everythin until %
+    {
         if(*DATA == 0){
           DATA = (int *)data_adr;
-          return;
+          goto end_loop;
         }
         //*free = c;
         //free++;
@@ -160,13 +174,25 @@ void analyze(){
     //free++;
     DATA++;//read TYPE (*useless line can be deleted later*)
 
-    //go to end of the message
-    la = DATA+2;
-    while (*la!='%')
-    {
-      DATA++;
-      la++;
+    la = DATA+4; 
+    if(*la=='B' && *(la+1)=='r' && *(la+2)=='c' && *(la+3)=='s' && *(la+4)=='t'){
+      DATA++;// read
+      while (*la!='%')//go to end of the message
+      {
+        la++;
+      }
     }
+    else
+    {
+      la = DATA+2;
+      while (*la!='%')//go to end of the message
+      {
+        DATA++;
+        la++;
+      }
+    }
+    
+    
     //la == % and DATA == first value of RSSI
     //%ADDRESS,TYPE,,,C4      %
     //         ^free  ^DATA   ^la
@@ -186,9 +212,30 @@ void analyze(){
     free++;
     //distance = (20th root of 10)^((MEASURED_POWER-rssi))
     distance = pow_point(BASE,(int8_t)(MEASURED_POWER-rssi));
-    *free = (int8_t)(distance>>8);
+    int8_t integer = distance >>8;
+    *free = integer;
     free++;
     *free = (int8_t)(distance);
+    free++;
+    *free = ',';
+    free++;
+    //readable distance
+    //if(integer%100>0){
+    //  *free = '0'+(integer%100);
+    //  free++;
+    //}
+    //if(integer%10>0){
+    //  *free = '0'+(integer%10);
+    //  free++;
+//
+    //}
+    *free = '0'+integer;
+    free++;
+    *free = '.';
+    free++;
+    *free = '0'+(((uint8_t)(distance))>>7)*5;
+    free++;
+    *free = 'M';
     free++;
     *free = '%';
     free++;
@@ -207,9 +254,8 @@ void analyze(){
     rssi = 0;
     
   }
-    *free = '\0';
-    *la = '\0';
-    *DATA = '\0';
+  end_loop:
+    *free = 0;
   
     DATA = (int *)data_adr;
 }
